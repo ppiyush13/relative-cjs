3bb72ac9812dbf1e9012b6551dbe18a09f2db1dd
Piyush Lodaya <ppiyush13@gmail.com>
2021-06-10 22:29:10 +0530

    comment window specific tests

--- src/relative-cjs.test.ts
+++ src/relative-cjs.test.ts
@@ -4,11 +4,6 @@ describe("testing module", () => {
   it.each([
     ["./app", "./app", "./."],
     ["./a", "./b", "../b"],
-    [
-      "c:/windows/nodejs/path",
-      "d:/windows/nodejs/path",
-      "./d:/windows/nodejs/path",
-    ],
     ["a/1", "b/1", "../../b/1"],
     ["a", "b", "../b"],
     ["a/b/c", "a/b/c/d/e/f", "./d/e/f"],
